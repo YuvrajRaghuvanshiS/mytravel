b3a37f9636ca2228d88058d733bfc881c81500e6
Yuvraj Raghuvanshi <52948878+YuvrajRaghuvanshiS@users.noreply.github.com>
2025-04-05 07:31:06 +0000

    Add dummy users and travelOptions to db for reference

--- src/db.js
+++ src/db.js
@@ -1,3 +1,79 @@
 // In memory database
-export const users = {};
-export let travelOptions = [];
+export const users = {
+  yrs: {
+    id: "yrs",
+    name: "Yuvraj",
+    email: "yuvrajpr24@iitk.ac.in",
+    phone: "8126744708",
+    password: "$2b$10$I4Adw6Rw1xexMNMCcyv5V.j/Na.zYSFjw7vGeq.rOhaxpTJDC.mAa",
+  },
+};
+
+export let travelOptions = [
+  {
+    id: 1,
+    agencyId: "sbasia",
+    type: "train",
+    source: "Boston",
+    destination: "Chicago",
+    date: "2025-04-10T00:00:00.000Z",
+    departureTime: "2025-04-10T08:00:00.000Z",
+    arrivalTime: "2025-04-10T16:00:00.000Z",
+    reachTime: "2025-04-10T16:30:00.000Z",
+    basePrice: 120,
+    seats: [
+      {
+        seatNumber: "1A",
+        price: 150,
+        booked: false,
+        passenger: null,
+      },
+      {
+        seatNumber: "1B",
+        price: 130,
+        booked: false,
+        passenger: null,
+      },
+      {
+        seatNumber: "2A",
+        price: 120,
+        booked: false,
+        passenger: null,
+      },
+    ],
+    availableSeats: 3,
+  },
+  {
+    id: 2,
+    agencyId: "yrs",
+    type: "train",
+    source: "Boston",
+    destination: "Chicago",
+    date: "2025-04-10T00:00:00.000Z",
+    departureTime: "2025-04-10T08:00:00.000Z",
+    arrivalTime: "2025-04-10T16:00:00.000Z",
+    reachTime: "2025-04-10T16:30:00.000Z",
+    basePrice: 120,
+    seats: [
+      {
+        seatNumber: "1A",
+        price: 150,
+        booked: false,
+        passenger: null,
+      },
+      {
+        seatNumber: "1B",
+        price: 130,
+        booked: false,
+        passenger: null,
+      },
+      {
+        seatNumber: "2A",
+        price: 120,
+        booked: false,
+        passenger: null,
+      },
+    ],
+    availableSeats: 3,
+  },
+]; // Stores travel options added by agencies

a854f935959c05276e5363fcc09672c1c2ad3890
Yuvraj Raghuvanshi <52948878+YuvrajRaghuvanshiS@users.noreply.github.com>
2025-04-23 18:15:04 +0000

    Update default values of booking, doesn't matter, I have OCD

--- chaincode/src/booking.ts
+++ chaincode/src/booking.ts
@@ -7,22 +7,22 @@ import { Object, Property } from "fabric-contract-api";
 @Object()
 export class Booking {
   @Property()
-  public bookingID: string = "First Booking";
+  public bookingID: string = "book_1745430810..._vxcx98...";
 
   @Property()
-  public userID: string = "First User";
+  public userID: string = "yuvrajRaghuvanshiS";
 
   @Property()
   public isUserAnonymous: boolean = true;
 
   @Property()
-  public userName: string = "User name";
+  public userName: string = "Yuvraj Raghuvanshi";
 
   @Property()
-  public userEmail: string = "user@user.com";
+  public userEmail: string = "not.my.email@example.com";
 
   @Property()
-  public travelID: string = "First Travel";
+  public travelID: string = "1";
 
   @Property()
   public seatNumbers: string = "1A,1B";
@@ -31,7 +31,7 @@ export class Booking {
   public totalPrice: number = 100;
 
   @Property()
-  public transactionID: string = "First Transaction";
+  public transactionID: string = "txn_1745430810..._e4sorr...";
 
   @Property()
   public status: string = "Confirmed";
@@ -40,5 +40,6 @@ export class Booking {
   public createdAt: string = new Date().toISOString();
 
   @Property()
-  public hyperledgerTxId: string = "";
+  public hyperledgerTxId: string =
+    "8a0f66758c9ece3154d68fa173f9403de42ba6955f1e690e3de5df5f031c2...";
 }

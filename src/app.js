c6c0ce7078676fe75ff939368761944bf38bc74c
Yuvraj Raghuvanshi <52948878+YuvrajRaghuvanshiS@users.noreply.github.com>
2025-04-24 06:13:21 +0000

    Add CORS middleware

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import travelRoutes from "./routes/travelRoutes.js";
 import agencyRoutes from "./routes/agencyRoutes.js";
 import walletRoutes from "./routes/walletRoutes.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
